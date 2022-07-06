0845b9772b0a2892d0dd590afd1efb94e73b3a84
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2022-07-06 12:35:08 +0530

    Add files via upload

---
+++ GettingStarted/Model/OrderInfoRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+
+namespace GettingStarted
+{
+    public class OrderInfoRepository
+    {
+        private ObservableCollection<OrderInfo> orderInfo;
+        public ObservableCollection<OrderInfo> OrderInfoCollection
+        {
+            get { return orderInfo; }
+            set { this.orderInfo = value; }
+        }
+
+        public OrderInfoRepository()
+        {
+            orderInfo = new ObservableCollection<OrderInfo>();
+            this.GenerateOrders();
+        }
+
+        public void GenerateOrders()
+        {
+            orderInfo.Add(new OrderInfo("1001", "Maria Anders", "Germany", "ALFKI", "Berlin"));
+            orderInfo.Add(new OrderInfo("1002", "Ana Trujillo", "Mexico", "ANATR", "Mexico D.F."));
+            orderInfo.Add(new OrderInfo("1003", "Ant Fuller", "Mexico", "ANTON", "Mexico D.F."));
+            orderInfo.Add(new OrderInfo("1004", "Thomas Hardy", "UK", "AROUT", "London"));
+            orderInfo.Add(new OrderInfo("1005", "Tim Adams", "Sweden", "BERGS", "London"));
+            orderInfo.Add(new OrderInfo("1006", "Hanna Moos", "Germany", "BLAUS", "Mannheim"));
+            orderInfo.Add(new OrderInfo("1007", "Andrew Fuller", "France", "BLONP", "Strasbourg"));
+            orderInfo.Add(new OrderInfo("1008", "Martin King", "Spain", "BOLID", "Madrid"));
+            orderInfo.Add(new OrderInfo("1009", "Lenny Lin", "France", "BONAP", "Marsiella"));
+            orderInfo.Add(new OrderInfo("1010", "John Carter", "Canada", "BOTTM", "Lenny Lin"));
+            orderInfo.Add(new OrderInfo("1011", "Laura King", "UK", "AROUT", "London"));
+            orderInfo.Add(new OrderInfo("1012", "Anne Wilson", "Germany", "BLAUS", "Mannheim"));
+            orderInfo.Add(new OrderInfo("1013", "Martin King", "France", "BLONP", "Strasbourg"));
+            orderInfo.Add(new OrderInfo("1014", "Gina Irene", "UK", "AROUT", "London"));
+        }
+    }
+
+
+}
